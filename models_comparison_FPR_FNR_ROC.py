017bb5ce7d7d257facc4b016eeadb95d3ff31dbc
snehas212 <sanamlaptop@gmail.com>
2024-09-07 21:18:46 +0100

    Update and rename models_comparison_FPR_FNR_ROC.txt to models_comparison_FPR_FNR_ROC.py

--- models_comparison_FPR_FNR_ROC.py
+++ models_comparison_FPR_FNR_ROC.py
@@ -21,8 +21,8 @@ if not os.path.exists('results'):
 logging.basicConfig(filename='results/final_fpr_fnr__model_comparison.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
 # Load datasets
-train = pd.read_csv('/home/vaithees/PhD-Projects/Sentiment Analysis/Datasets/drugs_review/drugsComTrain_raw.tsv', delimiter='\t')
-test = pd.read_csv('/home/vaithees/PhD-Projects/Sentiment Analysis/Datasets/drugs_review/drugsComTest_raw.tsv', delimiter='\t')
+train = pd.read_csv('/home/sneha/Sentiment Analysis/Datasets/drugs_review/drugsComTrain_raw.tsv', delimiter='\t')
+test = pd.read_csv('/home/sneha/Sentiment Analysis/Datasets/drugs_review/drugsComTest_raw.tsv', delimiter='\t')
 
 # Preprocessing
 tfidf = TfidfVectorizer(stop_words='english', max_features=1000)
